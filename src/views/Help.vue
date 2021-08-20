b243ddcf30ed14cd46bcc2a697282d2dc7ef7acb
Tamda <abdetam452@gmail.com>
2021-08-21 00:33:57 +0200

    Added Help title

---
+++ src/views/Help.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="man">
+        <h1>this is my page help</h1>
+        <p>here i testing my header vue</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Help",
+};
+</script>
+<style scoped>
+h1 {
+    text-transform: capitalize;
+    color: green;
+    font-size: 40px;
+}
+</style>
