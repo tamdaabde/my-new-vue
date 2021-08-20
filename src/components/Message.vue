aae57b1e210db5c5e268222df7e67a0689893acf
Tamda <abdetam452@gmail.com>
2021-08-21 00:07:26 +0200

    Testing Project vue cli

---
+++ src/components/Message.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="dimo">
+        <h1>this is my page vue</h1>
+        <p>i try to create my first component</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Message",
+};
+</script>
+<style scoped>
+h1 {
+    text-transform: uppercase;
+    color: #f00;
+}
+p {
+    text-transform: capitalize;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <Message />
+    </div>
 </template>
+<script>
+import Message from "@/components/Message.vue";
+export default {
+    name: "About",
+    components: {
+        Message,
+    },
+};
+</script>
